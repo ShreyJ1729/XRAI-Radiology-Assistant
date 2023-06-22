15ee7c53e749a9f7f6e913ae647b99a8fc1dbfa7
Shrey Joshi <shreyjoshi2004@gmail.com>
2023-06-22 00:04:21 -0500

    final push

--- src/App.js
+++ src/App.js
@@ -56,7 +56,9 @@ function App() {
   const [indexForGrad, setIndexForGrad] = useState(0);
   const [rep, setRep] = useState("");
   const [filename, setFilename] = useState("");
+  const [pdfloading, setPdfloading] = useState(false);
   const SIZE_OF_IMAGE = 600;
+  const OPENAI_KEY = "YOUR KEY HERE";
 
   let global_img_input = null;
 
@@ -341,6 +343,7 @@ function App() {
 
       thispred.logits = logits;
       thispred.classes = await distOverClasses(logits);
+      window.classes = thispred.classes;
 
       // from thispred.classes, create a dict of names --> probs
       let probs = {};
@@ -357,7 +360,8 @@ function App() {
       setScores(probs);
 
       const filename = imgElement.src.split("/").pop();
-      showProbResults(filename); //, logits, recScore)
+      window.filename = filename;
+      // showProbResults(filename); //, logits, recScore)
       setLoading(false);
       setEnd(Date.now());
 
@@ -417,11 +421,11 @@ function App() {
           {
             role: "system",
             content:
-              "You are a helpful medical education bot that thoroughly analyzes conditions and probabilities and returns information about the patient.",
+              "You are a helpful medical education bot that thoroughly analyzes conditions and probabilities about thoracic diseases from chest x-ray data and returns information about the patient.",
           },
           {
             role: "user",
-            content: `Given this table of conditions and probabilities ${tableString}, give me a diagnosis and rationale. Also give me what the next steps are for the patient."`,
+            content: `Given this table of pulmonary conditions and relevant probabilites: ${tableString}, give me 1) a diagnosis for the patient, 2) a rationale for your diagnosis, 3) potentially correlated conditions, and 4) a possible treatment plan. Clearly delinieate each section and use the table to support your answers. Include a paragraph break betwee each and also inclulde a disclaimer at the end asking the patient to consult a doctor for a formal diagnosis."`,
           },
         ],
         model: "gpt-3.5-turbo",
@@ -432,7 +436,7 @@ function App() {
       {
         headers: {
           "Content-Type": "application/json",
-          Authorization: `Bearer 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`,
+          Authorization: `Bearer ${OPENAI_KEY}`,
         },
       }
     );
@@ -441,61 +445,61 @@ function App() {
   };
 
   function addNewlines(res) {
-    const paragraphs = res.split('\n');
-    let result = '';
+    const paragraphs = res.split("\n");
+    let result = "";
 
     paragraphs.forEach((paragraph, i) => {
       const words = paragraph.split(/\s+/);
       let lineLength = 0;
 
-      words.forEach(word => {
+      words.forEach((word) => {
         if (lineLength + word.length > 120) {
-          result += '\n';
+          result += "\n";
           lineLength = 0;
         }
-        result += word + ' ';
+        result += word + " ";
         lineLength += word.length + 1;
       });
 
       if (i < paragraphs.length - 1) {
-        result += '\n';
+        result += "\n";
       }
     });
 
     return result.trim();
   }
 
-  async function showProbResults(filename) {
-    let classes = thispred.classes;
-    
+  async function showProbResults() {
+    setPdfloading(true);
+    let filename = window.filename;
+    let classes = window.classes;
+    console.log("classes=" + classes);
+
     const doc = new jsPDF();
-    
+
     doc.setFontSize(20);
     doc.setFont("helvetica", "bold");
-    
+
     doc.setFillColor(255, 140, 105);
     doc.rect(0, 0, 210, 18, "F");
 
     doc.addImage(logo, "PNG", 155, 2, 45, 15);
-    
+
     doc.setTextColor(255, 255, 255);
     doc.text("Take Home Diagnosis Report", 15, 12);
 
+    const datestring = new Date().toLocaleDateString("en-US", {
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+    });
 
-    const datestring =
-      new Date().toLocaleDateString("en-US", {
-        month: "long",
-        day: "numeric",
-        year: "numeric",
-      })
-    
-    const timestring =
-      new Date().toLocaleTimeString("en-US", {
-        hour: "numeric",
-        minute: "numeric",
-        hour12: true,
-      })
-      
+    const timestring = new Date().toLocaleTimeString("en-US", {
+      hour: "numeric",
+      minute: "numeric",
+      hour12: true,
+    });
+
     doc.setFontSize(10);
     doc.setTextColor(0, 0, 0);
     doc.setFont("helvetica", "normal");
@@ -503,18 +507,22 @@ function App() {
     doc.text("Date: " + datestring, 10, 25);
     doc.text("Time: " + timestring, 10, 30);
     doc.text("Filename: " + filename, 10, 35);
-    
+
     doc.text("\n\n\n\n\n\n\n\n", 10, 20);
-    
+
     let table = [];
     let tableString = "";
     for (let i = 0; i < classes.length; i++) {
       if (classes[i].className !== "None" && classes[i].className !== "") {
-        table.push([classes[i].className, (classes[i].probability * 100).toFixed(2) + "%"]);
-        tableString += classes[i].className + ": " + classes[i].probability * 100 + "%\n";
+        table.push([
+          classes[i].className,
+          (classes[i].probability * 100).toFixed(2) + "%",
+        ]);
+        tableString +=
+          classes[i].className + ": " + classes[i].probability * 100 + "%\n";
       }
     }
-    
+
     doc.autoTable({
       startY: 45,
       head: [["Diagnosis", "Probability"]],
@@ -526,15 +534,21 @@ function App() {
       res = "Diagnosis in progress!";
     }
 
-    res = addNewlines(res)
+    res = addNewlines(res);
     doc.text(res, 10, 160);
-  
+
     doc.setFontSize(7);
-    doc.text("© Copyright 2023 XRAI. All rights reserved.\nAll your data is stored privately and encrypted.", 200, 285, { align: "right" });
+    doc.text(
+      "© Copyright 2023 XRAI. All rights reserved.\nAll your data is stored privately and encrypted.",
+      200,
+      285,
+      { align: "right" }
+    );
+
+    setPdfloading(false);
 
     doc.save(Date.now() + "_diagnosis.pdf");
   }
-
 
   function showProbError(predictionContainer, score) {
     alert(
@@ -775,7 +789,7 @@ function App() {
       {
         headers: {
           "Content-Type": "application/json",
-          Authorization: `Bearer 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`,
+          Authorization: `Bearer ${OPENAI_KEY}`,
         },
       }
     );
@@ -939,11 +953,19 @@ function App() {
                 size="sm"
                 mr={0}
                 ml="auto"
-                onClick={() => {}}
+                onClick={() => {
+                  showProbResults(filename, thispred.classes);
+                }}
               >
-                {"Export PDF Report"}
-                <Spacer />
-                <FaExternalLinkAlt></FaExternalLinkAlt>
+                {pdfloading ? (
+                  <Spinner />
+                ) : (
+                  <>
+                    "Export PDF Report"
+                    <Spacer />
+                    <FaExternalLinkAlt></FaExternalLinkAlt>
+                  </>
+                )}
               </Button>
             </HStack>
             <HStack>
